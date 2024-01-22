437b4efb891db40fc495300742f81d20f757b265
Erickb <wr.t8r1@gmai.com>
2024-01-22 12:40:11 -0800

    add post /orders endpoint

--- pages/api/v1/orders/[...orderId].ts
+++ pages/api/v1/orders/[...orderId].ts
@@ -5,62 +5,6 @@ import { prisma } from "@/lib/prisma";
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === "POST") {
-      try {
-          const {
-              name,
-              orderType,
-              topic,
-              description,
-              subject,
-              pages,
-              words,
-              clientDeadline,
-              writerDeadline,
-              amountReceived,
-              educationLevel,
-              status,
-              userId,
-              assignedById,
-              clientId
-          } = req.body;
-  
-          console.log(req.body);
-  
-  
-          if(!name || !orderType || !topic || !description || !subject || !pages || !words || !clientDeadline || !writerDeadline || !educationLevel || !status || !userId || !assignedById || !clientId){
-              throw new Error('All fields are required');
-          }
-  
-        
-          const order: any= await prisma.order.create({
-              data: {
-                  name,
-                  orderType,
-                  topic,
-                  description,
-                  subject,
-                  pages,
-                  words,
-                  clientDeadline,
-                  writerDeadline,
-                  amountReceived,
-                  educationLevel,
-                  status,
-                  userId,
-                  assignedById,
-                  clientId
-              }});
-  
-          if(!order){
-              throw new NotFoundError('Order not created');
-          }
-          successResponse(res, order, 201);
-  
-      } catch (error: any) {
-          failureResponse(res, error.message);
-      }
-    }
     if (req.method === "GET") {
       try {
         const orderId = req.url?.split('orders/')[1];
