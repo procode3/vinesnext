01fee0d7ef858345704fbdf3cd24865666fb168d
Erickb <wr.t8r1@gmai.com>
2024-02-22 05:11:40 -0800

    update 2.0

--- app/(auth)/signin/layout.tsx
+++ app/(auth)/signin/layout.tsx
@@ -8,7 +8,10 @@ type LayoutProps = {
   session: any
 };
 
-function LoginLayout({ children, session }: LayoutProps) {
+function LoginLayout({ children, session }: Readonly<{
+  children: React.ReactNode;
+  session: any;
+}>) {
   return (
     <div className='bg-white w-screen h-[90vh] flex flex-col items-center justify-center gap-4'>
       <NextTopLoader />
