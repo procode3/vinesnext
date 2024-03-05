5e811d03cd71d215e8fbdbca5d0b35d2508f742b
ggakila <gisorehaggai@gmail.com>
2024-03-06 00:31:16 +0300

    create order File issue fix

--- app/(dashboard)/(writer)/orders/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Neworders() {
-  return (
-    <div>New oders page</div>
-  )
-}
-
-export default Neworders
--- app/(dashboard)/(writer)/orders/revision/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Revision() {
-  return (
-    <div>Revision page</div>
-  )
-}
-
-export default Revision
