9d0407b81b4ff2ba29b8b39e715b6af9f8609584
ggakila <gisorehaggai@gmail.com>
2024-03-06 23:37:05 +0300

    types fix 3.0

---
+++ types/dialogportalprops.d.ts
@@ -0,0 +1,18 @@
+import { Portal as _Portal1 } from "@radix-ui/react-portal";
+
+type PortalProps = React.ComponentPropsWithoutRef<typeof _Portal1>;
+
+export interface DialogPortalProps {
+    children?: React.ReactNode;
+    className?: string;
+
+    /**
+     * Specify a container element to portal the content into.
+     */
+    container?: PortalProps['container'];
+    /**
+     * Used to force mounting when more control is needed. Useful when
+     * controlling animation with React animation libraries.
+     */
+    forceMount?: true;
+}
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import { User_type } from "@prisma/client"
+import type { IncomingMessage, ServerResponse } from 'http';
+
 declare module "next-auth" {
 
   interface Session {
@@ -9,4 +11,29 @@ declare module "next-auth" {
         userType: User_type
     }
   }
+
+  export interface NextApiRequestt extends IncomingMessage {
+    /**
+     * Object of `query` values from url
+     */
+    query: Partial<{
+        [key: string]: string | string[];
+    }>;
+    /**
+     * Object of `cookies` from header
+     */
+    cookies: Partial<{
+        [key: string]: string;
+    }>;
+    body: any;
+    env: Env;
+    draftMode?: boolean;
+    preview?: boolean;
+   files?:any;
+   file?:any;
+    /**
+     * Preview data set on the request, if any
+     * */
+    previewData?: PreviewData;
+}
 }
