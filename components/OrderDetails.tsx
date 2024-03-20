ecf91a57349bcfc8ded4d85ccc973f1218c13217
ggakila <gisorehaggai@gmail.com>
2024-03-20 21:43:44 +0300

    conflict resolve

--- components/OrderDetails.tsx
+++ components/OrderDetails.tsx
@@ -24,64 +24,64 @@ export default function OrderDetails({ order }: OrderHeaderProps) {
 				<div className="flex flex-col md:flex-row px-20">
 					<div className="flex flex-col w-full md:w-1/2">
 						<div className="flex justify-between items-center">
-							<div className="p-2 font-semibold text-xs ">Subject:</div>
-							<div className="p-2 font-light text-xs">
+							<div className="p-2 font-semibold text-[14px] ">Subject:</div>
+							<div className="p-2 font-light text-[14px]">
 								{`${order?.subject.charAt(0).toUpperCase() + order?.subject.slice(1).toLowerCase()}`}
 							</div>
 						</div>
 
 
 						<div className="flex justify-between items-center">
-							<div className="p-2 font-semibold text-xs ">Pages:</div>
-							<div className="p-2 font-light text-xs ">{order?.pages}</div>
+							<div className="p-2 font-semibold text-[14px] ">Pages:</div>
+							<div className="p-2 font-light text-[14px] ">{order?.pages}</div>
 						</div>
 
 						<div className="flex justify-between items-center">
-							<div className="p-2 font-semibold text-xs ">Order Type:</div>
-							<div className="p-2 font-light text-xs ">
+							<div className="p-2 font-semibold text-[14px] ">Order Type:</div>
+							<div className="p-2 font-light text-[14px] ">
 								{`${order?.orderType.charAt(0).toUpperCase() + order?.orderType.slice(1).toLowerCase()}`}
 							</div>
 						</div>
 
 						<div className="flex justify-between items-center">
-							<div className="p-2 font-semibold text-xs ">Sources:</div>
-							<div className="p-2 font-light text-xs ">
+							<div className="p-2 font-semibold text-[14px] ">Sources:</div>
+							<div className="p-2 font-light text-[14px] ">
 								{order?.sources}
 							</div>
 						</div><div className="flex justify-between items-center">
-							<div className="p-2 font-semibold text-xs ">Language Style:</div>
-							<div className="p-2 font-light text-xs ">
+							<div className="p-2 font-semibold text-[14px] ">Language Style:</div>
+							<div className="p-2 font-light text-[14px] ">
 								English(US)
 							</div>
 						</div>
 
 
 						<div className="flex justify-between items-center">
-							<div className="p-2 font-semibold text-xs ">
+							<div className="p-2 font-semibold text-[14px] ">
 								Education Level:
 							</div>
-							<div className="p-2 font-light text-xs ">
+							<div className="p-2 font-light text-[14px] ">
 								{`${order?.educationLevel.charAt(0).toUpperCase() + order?.educationLevel.slice(1).toLowerCase()}`}
 							</div>
 						</div>
 
 						<div className="flex justify-between items-center">
-							<div className="p-2 font-semibold text-xs ">Words:</div>
-							<div className="p-2 font-light text-xs ">
+							<div className="p-2 font-semibold text-[14px] ">Words:</div>
+							<div className="p-2 font-light text-[14px] ">
 								{order?.words}
 							</div>
 						</div>
 
 						<div className="flex justify-between items-center">
-							<div className="p-2 font-semibold text-xs ">Citation Style:</div>
-							<div className="p-2 font-light text-xs ">
+							<div className="p-2 font-semibold text-[14px] ">Citation Style:</div>
+							<div className="p-2 font-light text-[14px] ">
 								{order?.citationStyle}
 							</div>
 						</div>
 
 						<div className="flex justify-between items-center">
-							<div className="p-2 font-semibold text-xs ">Spacing:</div>
-							<div className="p-2 font-light text-xs ">
+							<div className="p-2 font-semibold text-[14px] ">Spacing:</div>
+							<div className="p-2 font-light text-[14px] ">
 								{`${order?.spacing.charAt(0).toUpperCase() + order?.spacing.slice(1).toLowerCase()}`}
 							</div>
 						</div>
@@ -91,8 +91,8 @@ export default function OrderDetails({ order }: OrderHeaderProps) {
 					<div className="flex flex-col w-full md:w-1/2">
 
 						<div className="w-full">
-							<div className="p-2 font-semibold text-xs ">Description:</div>
-							<div className="p-2 font-light text-xs ">
+							<div className="p-2 font-semibold text-[14px] ">Description:</div>
+							<div className="p-2 font-light text-[14px] ">
 								{order?.description}
 							</div>
 						</div>
